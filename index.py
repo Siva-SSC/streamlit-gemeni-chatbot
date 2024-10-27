cb0dddb3ab2b26f05d1b93b37c68a4b365c5048a
Siva_ <74825801+Siva-SSC@users.noreply.github.com>
2024-10-27 21:21:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-import google.generativeai as genai
+import streamlit as st
+import google.generativeai as gen_ai
+GOOGLE_API_KEY = "AIzaSyDNbLonDcxyJjvmKMQOmmU2_GyeH50hTG0"
+gen_ai.configure(api_key=GOOGLE_API_KEY)
+model = gen_ai.GenerativeModel('gemini-1.5-flash')
+text = st.text_input("Give your prompt")
+st.write(model.generate_content(text).text)
